18ce497733ee620f961f86f5db3c53725f6ab852
Mamo YZ <yzmamo@gmail.com>
2023-03-05 01:52:17 +0300

    basic styles for Header component

--- components/AppHeader/index.vue
+++ components/AppHeader/index.vue
@@ -1,15 +1,21 @@
 <template>
-	<div>
-		<div id="app-logo">ExmoxAssessment</div>
-		<div>
-			<form id="search-bar">
-				<input type="text" name="" id="" />
-			</form>
+	<header class="py-10 fixed bg-transparent text-white top-0 w-full left-0 right-0 border-b-2 border-gray-700">
+		<div class="container">
+			<div class="flex flex-row justify-between items-center">
+				<div>
+					<div id="app-logo">ExmoxAssessment</div>
+				</div>
+				<div class="flex items-center gap-10">
+					<div>
+						<form id="search-bar">
+							<input type="text" name="" id="" />
+						</form>
+					</div>
+					<nuxt-link to="/" id="favorites-button">Favorites</nuxt-link>
+				</div>
+			</div>
 		</div>
-		<div>
-			<nuxt-link to="/" id="favorites-button">Favorites</nuxt-link>
-		</div>
-	</div>
+	</header>
 </template>
 
 <script>
