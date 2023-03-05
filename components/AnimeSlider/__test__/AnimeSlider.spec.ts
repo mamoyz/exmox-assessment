b75824a071040f32529eebcec589cc839ab754ad
Mamo YZ <yzmamo@gmail.com>
2023-03-05 14:07:28 +0300

    added tests for AnimeSlider component

---
+++ components/AnimeSlider/__test__/AnimeSlider.spec.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+import AnimeSlider from "../index.vue";
+import mockAnimes from "../../../mocks/mockAnimes.json";
+describe("Testing AnimeSlider Component", () => {
+	let wrapper: any = null;
+	beforeEach(() => {
+		wrapper = shallowMount(AnimeSlider, {
+			props: {
+				title: "Test",
+				animes: mockAnimes,
+			},
+		});
+	});
+	it("renders the component", () => {
+		expect(wrapper.exists()).toBeTruthy();
+	});
+	it("renders next/prev buttons", () => {
+		expect(wrapper.find(".next").exists()).toBeTruthy();
+		expect(wrapper.find(".prev").exists()).toBeTruthy();
+	});
+	it("renders title", () => {
+		expect(wrapper.find(".title").exists()).toBeTruthy();
+		expect(wrapper.find(".title").text()).toBe("Test");
+	});
+	it("renders anime list item", () => {
+		expect(wrapper.findComponent({ name: "AnimeListItem" }).exists()).toBeTruthy();
+		expect(wrapper.findAllComponents({ name: "AnimeListItem" }).length).toBe(5);
+	});
+});
