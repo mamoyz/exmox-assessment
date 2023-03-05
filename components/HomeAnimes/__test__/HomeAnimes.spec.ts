e4b5414d48f17dbf446b85daee9bc7ad8a66c688
Mamo YZ <yzmamo@gmail.com>
2023-03-05 13:33:36 +0300

    added HomeAnimes tests

---
+++ components/HomeAnimes/__test__/HomeAnimes.spec.ts
@@ -0,0 +1,19 @@
+import { it, describe, expect, beforeEach } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+import HomeAnimes from "../index.vue";
+
+describe("Testing HomeAnimes Component", () => {
+	let wrapper: any = null;
+	beforeEach(() => {
+		wrapper = shallowMount(HomeAnimes);
+	});
+
+	it("renders the component", () => {
+		expect(wrapper.exists()).toBeTruthy();
+	});
+
+	it("renders AnimeSlider component", () => {
+		expect(wrapper.findComponent({ name: "AnimeSlider" }).exists()).toBeTruthy();
+		expect(wrapper.findComponent({ name: "AnimeSlider" }).length).toBe(2);
+	});
+});
