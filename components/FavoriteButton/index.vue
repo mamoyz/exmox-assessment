682c8eb95b34298e4351983dc78cd3908114938c
Mamo YZ <yzmamo@gmail.com>
2023-03-05 18:10:47 +0300

    FavoriteButton passses tests

---
+++ components/FavoriteButton/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<button class="favorite-button" :class="{ active }"></button>
+</template>
+
+<script>
+	export default {
+		props: {
+			active: {
+				type: Boolean,
+				default: false,
+			},
+		},
+		setup(props) {
+			const { active } = toRefs(props);
+			return {
+				active,
+			};
+		},
+	};
+</script>
+
+<style lang="scss" scoped></style>
