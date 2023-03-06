b28553320e2bc9ab5dc74809d0ffe656827c9a63
Mamo YZ <yzmamo@gmail.com>
2023-03-06 12:39:26 +0300

    HomeAnimes tests

--- components/HomeAnimes/__test__/HomeAnimes.spec.ts
+++ components/HomeAnimes/__test__/HomeAnimes.spec.ts
@@ -1,7 +1,7 @@
 import { it, describe, expect, beforeEach } from "vitest";
 import { shallowMount } from "@vue/test-utils";
 import HomeAnimes from "../index.vue";
-
+import mockAnimes from "../../../mocks/mockAnimes.json";
 describe("Testing HomeAnimes Component", () => {
 	let wrapper: any = null;
 	beforeEach(() => {
@@ -15,5 +15,17 @@ describe("Testing HomeAnimes Component", () => {
 	it("renders AnimeSlider component", () => {
 		expect(wrapper.findComponent({ name: "AnimeSlider" }).exists()).toBeTruthy();
 		expect(wrapper.findAllComponents({ name: "AnimeSlider" }).length).toBe(2);
+	});
+	it("loads props", () => {
+		const wrapperWithProps = shallowMount(HomeAnimes, {
+			props: {
+				animes: {
+					trending: mockAnimes,
+					top: mockAnimes,
+				},
+			},
+		});
+		expect(wrapperWithProps.vm.animes.trending.length).toBe(5);
+		expect(wrapperWithProps.vm.animes.trending.top).toBe(5);
 	});
 });
