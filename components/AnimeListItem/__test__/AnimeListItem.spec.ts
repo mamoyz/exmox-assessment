35f324738da36bf5f1debd433f2d06b78b6c8143
Mamo YZ <yzmamo@gmail.com>
2023-03-06 16:10:48 +0300

    updated tests for AnimeListItem

--- components/AnimeListItem/__test__/AnimeListItem.spec.ts
+++ components/AnimeListItem/__test__/AnimeListItem.spec.ts
@@ -14,6 +14,12 @@ describe("Testing AnimeListItem Component", () => {
 	it("renders the component", () => {
 		expect(wrapper.exists()).toBeTruthy();
 	});
+
+	it("renders anime link", () => {
+		expect(wrapper.find(".anime-link").exists()).toBeTruthy();
+		expect(wrapper.find(".anime-link").attributes("href")).toBe("/anime/blue-lock/137822");
+	});
+
 	it("renders anime title", () => {
 		expect(wrapper.find(".title").exists()).toBeTruthy();
 		expect(wrapper.find(".title").text()).toBe("Blue Lock");
@@ -34,10 +40,6 @@ describe("Testing AnimeListItem Component", () => {
 		expect(wrapper.find(".genres").text()).toBe("Action, Drama, Sports");
 	});
 
-	it("renders anime description", () => {
-		expect(wrapper.find(".description").exists()).toBeTruthy();
-		expect(wrapper.find(".description").text()).toBe("After a disastrous defeat at the 2018 World Cup, Japan's team struggles to regroup. But what's missing? An absolute Ace Striker, who can guide them to the win. The Japan Football Union is hell-bent on creating a striker who hungers for goals and thirsts for victory, and who can be the decisive instrument in turning around a losing match...and to do so, they've gathered 300 of Japan's best and brightest youth players. Who will emerge to lead the team...and will they be able to out-muscle and out-ego everyone who stands in their way? <br>\n<br>\n(Source: Kodansha USA)<br>\n<br>\n<i>Note: The first episode received a pre-screening at the AnimagiC convention on August 6, 2022.</i>");
-	});
 	it("renders anime airing status", () => {
 		expect(wrapper.find(".status").exists()).toBeTruthy();
 	});
