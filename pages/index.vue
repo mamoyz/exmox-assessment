f4b576c78297c8ada9a71c2c267f0109353b8ce8
Mamo YZ <yzmamo@gmail.com>
2023-03-06 11:29:01 +0300

    homepage passes fetch data tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,38 @@
 <template>
 	<div>
-		<HomeHero />
+		<HomeHero :featuredAnime="featuredAnime" />
 		<HomeAnimes />
 	</div>
 </template>
 
 <script>
-	import HomeHero from "@/components/HomeHero/index.vue";
-	import HomeAnimes from "@/components/HomeAnimes/index.vue";
+	import HomeHero from "~/components/HomeHero/index.vue";
+	import HomeAnimes from "~/components/HomeAnimes/index.vue";
+	import { useAnimeStore } from "~/stores/animes";
+
 	export default {
+		name: "HomePage",
 		components: {
 			HomeHero,
 			HomeAnimes,
+		},
+		setup() {
+			const animeStore = useAnimeStore();
+			const animes = ref();
+			const fetchHomeAnimes = async () => {
+				const homeAnimes = await animeStore.getHomeAnimes();
+				animes.value = homeAnimes;
+			};
+			const featuredAnime = computed(() => {
+				return animes.value?.data?.trending?.media[0];
+			});
+			onMounted(() => {
+				setTimeout(() => {
+					fetchHomeAnimes();
+				}, 0);
+			});
+
+			return { animes, fetchHomeAnimes, featuredAnime };
 		},
 	};
 </script>
