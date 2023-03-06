600460f9e502a29383c6e05e0c5b8160d60c36a6
Mamo YZ <yzmamo@gmail.com>
2023-03-06 21:57:29 +0300

    watchlist page passes tests

--- pages/my-watchlist/index.vue
+++ pages/my-watchlist/index.vue
@@ -2,10 +2,13 @@
 	<section class="pt-48 pb-24">
 		<div class="container">
 			<h1 class="title text-5xl text-white font-bold">My Watchlist</h1>
-			<div class="flex flex-wrap -mx-3">
+			<div v-show="watchList?.length" class="flex flex-wrap -mx-3">
 				<div class="w-full md:w-6/12 lg:w-3/12 px-3 mt-14" :key="item.id" v-for="item in watchList">
 					<AnimeListItem :anime="item" />
 				</div>
+			</div>
+			<div v-show="!watchList || watchList.length === 0" class="not-found mt-6">
+				<h2 class="text-2xl text-white">No animes found in your watchlist</h2>
 			</div>
 		</div>
 	</section>
